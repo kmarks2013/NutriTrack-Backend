a499662cbc3b69213f5e9e3cf019dec0870007f4
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-11-26 15:42:01 -0500

    Bugs fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post "/login", to: "auth#login"
   post '/signup', to: 'users#create'
   get '/profile', to: 'users#profile'
+  # patch '/update', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
