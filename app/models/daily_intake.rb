b51b1dc82a496bf46d211f15850707be970e1ae9
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-12-03 21:11:52 -0500

    Changed math from backend to frontend

--- app/models/daily_intake.rb
+++ app/models/daily_intake.rb
@@ -51,37 +51,37 @@ class DailyIntake < ApplicationRecord
         return remaining
     end
 
-    def user_remaining_fat
-        remaining= 0
-        if self.user.goal_fat
-        remaining = self.user.goal_fat - self.total_fat
-        end
-        return remaining.round(2)
-     end
+    # def user_remaining_fat
+    #     remaining= 0
+    #     if self.user.goal_fat
+    #     remaining = self.user.goal_fat - self.total_fat
+    #     end
+    #     return remaining.round(2)
+    #  end
 
-     def user_remaining_carbs
-        remaining= 0
-        if self.user.goal_carbs
-        remaining = self.user.goal_carbs - self.total_carbs
-        end
-        return remaining.round(2)
-     end
+    #  def user_remaining_carbs
+    #     remaining= 0
+    #     if self.user.goal_carbs
+    #     remaining = self.user.goal_carbs - self.total_carbs
+    #     end
+    #     return remaining.round(2)
+    #  end
 
-     def user_remaining_protein
-        remaining= 0
-        if self.user.goal_protein
-        remaining = self.user.goal_protein - self.total_protein
-        end
-        return remaining.round(2)
-     end
+    #  def user_remaining_protein
+    #     remaining= 0
+    #     if self.user.goal_protein
+    #     remaining = self.user.goal_protein - self.total_protein
+    #     end
+    #     return remaining.round(2)
+    #  end
 
-     def user_remaining_sugar
-        remaining= 0
-        if self.user.goal_sugar
-        remaining = self.user.goal_sugar - self.total_sugar
-        end
-        return remaining.round(2)
-     end
+    #  def user_remaining_sugar
+    #     remaining= 0
+    #     if self.user.goal_sugar
+    #     remaining = self.user.goal_sugar - self.total_sugar
+    #     end
+    #     return remaining.round(2)
+    #  end
 
     def changed_date
         splitted_date = self.date.to_s.split('-')
